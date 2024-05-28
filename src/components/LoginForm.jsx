9514decb76f62a4c050cd15ed4f7f5f9e5a2f96b
jsdydaktyka <jsdydaktyka@gmail.com>
2024-05-28 10:00:38 +0200

    Add Login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,10 +1,45 @@
+import { useState } from "react"
+import axios from "axios"
+
 
 
 const LoginForm = () => {
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
 
+    function handleSubmit(e){
+        e.preventDefault()
+
+        console.log(`${userName}, ${password}`)
+
+    }
 
     return (
-        <h1>Login Form</h1>
+        <> 
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>user name:</label>
+                    <input 
+                        type="text"
+                        value={userName}
+                        onChange={(e)=>setUserName(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <div>
+                    <label>password:</label>
+                    <input 
+                        type="password"
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit"> Login </button>
+            </form>
+        
+        </>
     )
 }
 
