27e6d31b229f4b54658b9371c89a3da3ddf7c291
jsdydaktyka <jsdydaktyka@gmail.com>
2024-05-28 10:25:17 +0200

    Add button to navigate to login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes,useNavigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import WelcomePage from './pages/WelcomePage'
 
 function App() {
   const [count, setCount] = useState(0)
+  const navigate = useNavigate()
 
   return (
     <>
-     <h1>Aby się zalogować kliknij batona</h1>
+     <h4>Aby się zalogować kliknij batona</h4>
+     <button onClick={()=>navigate('/login')}>Klik</button>
      <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/welcome" element={<WelcomePage />} />
